d9a7b8f68b9557a27068d5abcaa18ee11c082a02
Naresh7143-reddy <nareshreddygodi777@gmail.com>
2025-12-29 09:23:11 +0530

    Prepare Django project for Render deployment

--- p1/settings.py
+++ p1/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2&enq#6^7l4q(8dq3l7(h0t8n0r-a(8-#x-40y7zt#4v6-6#p7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
